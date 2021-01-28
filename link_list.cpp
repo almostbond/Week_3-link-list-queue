a2db43f01836cf6dcb5a511e53b85cf2bf8e5ce7
almostbond <48605575+almostbond@users.noreply.github.com>
2021-01-27 16:07:45 -0800

    Add files via upload

--- link_list.cpp
+++ link_list.cpp
@@ -10,13 +10,15 @@ has a dequeue method that will appropriately remove an element from the front of
 Optionally has a peek method that returns the value at the front of the queue without removing it
 Bonus if you also create an array based Queue!*/
 
-#include <cstdlib>
+
 #include <iostream>
 #include "List2.h"
 
 using namespace std;
 
 
+
+// constructors.
 List::List() {
 
     head = NULL;
@@ -25,46 +27,64 @@ List::List() {
     temp = NULL;
 }
 
+Test::Test(){}
 
+// Creates a new node and assigns it a value and a spot at the back of the queue.
 void List::push(int add_val) {
 
-    struct node* new_node =(node*) malloc(sizeof(struct node));
+    node *new_node = new node;
 
-    new_node->next = head;
+    new_node->next = tail;
     new_node->value = add_val;
     if (head == NULL) {
-        new_node->previous = tail;
+        new_node->next = NULL;
+        head = new_node;
         tail = new_node;
     }
     else {
         
-        head->previous = new_node;
+        tail = new_node;
     }
     
-    head = new_node;
+   
 }
-
-void List::pop() {
-
+// deletes the eldest node and returns its value.
+int List::pop() {
     
+    int popped_val = NULL;
+    current = tail;
     temp = head;
-    cout << temp->value << endl;
-    head = head->next;
-    free(temp);
+    popped_val = temp->value;
+    // this feels like it would get very slow in the future if a large node was created.
+    while (current->next != temp) {
+
+        current = current->next;
+    }
+
+    
+    free(head);
+    head = current;
+    head->next = NULL;
     
 
+    cout << "Value popped: " << popped_val << endl;
+    cout << "Memory Address freed: " << temp << endl;
+
+    return popped_val;
 }
 
-
+// prints the head and tail of the queue.
 void List::peek() {
 
-    cout << head->value;
-    cout << tail->value;
+    cout << "Head value: " << head->value << endl;
+    cout << "Tail value: " << tail->value << endl;
 }
 
+
+//prints all node values in the stack.
 void List::print_list() {
 
-    current = head;
+    current = tail;
     while (current != NULL) {
         cout << current->value << endl;
         current = current->next;
@@ -73,37 +93,96 @@ void List::print_list() {
 }
 
 
+void List::menu() {
 
+    cout << "press 0 to quit" << endl;
+    cout << "press 1 to push" << endl;
+    cout << "press 2 to pop" << endl;
+    cout << "press 3 to peek" << endl;
 
-void List::insert(int value, int pos) {
-    //probably wont get around to doing this but I'll leave it here just in case.
-    int i = 0;
-    current = head;
+    int menu = 0;
     
+    
+    bool loop = true;
+    while (loop == true) {
+        cout << "Enter a number: ";
+        cin >> menu;
+        if (menu == 1) {
+            int node_val;
+            cout << "enter an integer value you'd like to store." << endl;
+            cin >> node_val;
+            push(node_val);
+            
+        }
+        if (menu == 2) {
+            pop();
+            
+        }
+        if (menu == 3) {
+            peek();
+            
+        }
+        if (menu == 0) {
+            loop = false;
+        }
+    }
+}
+
+// ~~~~testing stuff~~~~
+
+//test_1 just shows that the program can work.
+void Test::test_1() {
+
+    // populates the stack
+    for (int i = 0; i < 10; i++) {
+        
+        push(i);
+    }
+
+    //prints the stacks values
+    print_list();
+
+    //prints head and tail
+    peek();
+    for (int i = 0; i < 10; i++) {
+        pop();
+    }
+
+    print_list();
 
 }
+
+void Test::test_2() {
+    
+    for (int i = 0; i < 10; i++) {
+        
+        push(i);
+    }
+    // this will break stuff
+    for (int i = 0 ; i < 11; i++) {
+        pop();
+    }
+
+}
+
+
 
 int main()
 {
-    int menu = 0;
-    cin >> menu;
-    struct List link_list;
+    List listy;
+    Test testy;
+    // Uncomment for to manually use.
+    //listy.menu();
 
-    if (menu == 1) {
-        link_list.push(3);
-    }
-    if (menu == 2) {
-        link_list.pop();
-    }
-    if (menu == 3) {
-        link_list.peek();
-    }
-    if (menu == 0) {
-        return;
-    }
+    //Testing
+    cout << "starting test 1" << endl;
+    testy.test_1();
 
-    link_list.print_list();
+    cout << "starting test 2" << endl;
+    testy.test_2();//expected to crash
+    
 
-    std::cout << "Hello World!\n";
+    cout << "Successful Exit.\n";
 }
+
 
